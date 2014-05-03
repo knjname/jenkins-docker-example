0f02d8a8c7e24f7a055c094150e0b49bc0372c3f
knjname <knjname@example.com>
2014-05-03 17:06:06 +0900

    Add volume specification

--- master/dockerrun.sh
+++ master/dockerrun.sh
@@ -5,6 +5,8 @@ docker run \
     -p 8080:8080 \
     -p 10080:10080 \
     -e JENKINS_JNLP_PORT=10080 \
+    -v /opt/example-jenkins/logs:/jenkins/logs \
+    -v /opt/example-jenkins/home:/jenkins/home \
     -d \
     knjname/jenkins-master
 
